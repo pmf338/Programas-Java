6c7efe378011341a530158f69eaf2aa6d735f5e9
Pablo <pmf338@gmail.com>
2022-01-03 03:19:28 -0300

    Add files via upload

---
+++ Formulario/src/Logica/Formulario.java
@@ -0,0 +1,17 @@
+package Logica;
+
+import IGU.FormularioInscripcion;
+
+
+public class Formulario {
+
+    
+    public static void main(String[] args) {
+        
+        FormularioInscripcion formulario = new FormularioInscripcion ();
+        formulario.setVisible(true);
+        formulario.setLocationRelativeTo(null);
+        
+    }
+    
+}
