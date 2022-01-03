b4c1b371bb0c54f61dbd51a207eb1714ca7a384e
Pablo <pmf338@gmail.com>
2022-01-03 03:06:47 -0300

    Add files via upload

---
+++ Ferrari_Pablo_tpo1/AgendaElectronica/src/Logica/AgendaLogica.java
@@ -0,0 +1,19 @@
+package Logica;
+
+import IGU.AgendaElectronica;
+
+
+public class AgendaLogica {
+
+   
+    public static void main(String[] args) {
+        
+        AgendaElectronica agenda = new AgendaElectronica (); //creo mi pantalla
+        agenda.setVisible(true);// la hago visible
+        agenda.setLocationRelativeTo(null); //la centro en la pantalla
+        
+        
+        
+    }
+    
+}
