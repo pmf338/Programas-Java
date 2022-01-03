9edf7b1d8f4b4df3229e6ef87121020bd8ba1fa4
Pablo <pmf338@gmail.com>
2022-01-03 03:14:31 -0300

    Add files via upload

---
+++ Login/Login/src/Logica/Login.java
@@ -0,0 +1,16 @@
+package Logica;
+
+import IGU.LoginIGU;
+
+
+public class Login {
+
+    
+    public static void main(String[] args) {
+        
+        LoginIGU login = new LoginIGU ();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+    }
+    
+}
