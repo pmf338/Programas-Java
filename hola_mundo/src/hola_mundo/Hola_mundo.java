4165eaf2862ce8a54d85703c6eb6430ddd9448f5
Pablo <pmf338@gmail.com>
2022-01-03 03:08:36 -0300

    Add files via upload

---
+++ hola_mundo/src/hola_mundo/Hola_mundo.java
@@ -0,0 +1,23 @@
+package hola_mundo;
+
+
+public class Hola_mundo {
+
+    
+    public static void main(String[] args) //Metodo
+    {
+            
+        int numero = 4;
+        String perro = "Turi es mi perro";
+        double numeroConComa = 24.5;
+        boolean verdaderoOFalso = true; 
+        long numeroLargo = 999999;
+        char letra = 'a';
+        
+        System.out.println("hola mundo"); // sout(tabulacion)  Equivalente a printf 
+        
+        
+    
+    }
+    
+}
